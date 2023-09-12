2fd9b9c52a5d6bd63e5749fd23a86cd964062d7f
Guido Ceraso <gceraso@mrmilu.com>
2023-09-12 14:17:47 +0200

    feat(linter): test

--- packages/linter/src/entity_linter.ts
+++ packages/linter/src/entity_linter.ts
@@ -11,6 +11,7 @@ function nodeIsArray(node: TSESTree.PropertyDefinition): boolean {
     const _typeReference = typeReference as TSESTree.TSTypeReference;
     return (_typeReference.typeName as TSESTree.Identifier).name === "Array";
   }
+  // TEST
   return false;
 }
 
