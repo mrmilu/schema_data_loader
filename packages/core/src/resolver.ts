e8b46fef0ff2839a55cdfc5e6e01b3914bd10a1f
Guido Ceraso <gceraso@mrmilu.com>
2023-09-18 17:32:21 +0200

    fix(core): added resolver get options interface export

--- packages/core/src/resolver.ts
+++ packages/core/src/resolver.ts
@@ -1,5 +1,5 @@
 export { WithMeta } from "./models/with_meta";
 export { Union } from "./models/union";
 export type { IHttpClient } from "./interfaces/http_client";
-export type { IEntityResolverService } from "./interfaces/entity_resolver_service";
+export type { IEntityResolverService, ResolverGetOptions } from "./interfaces/entity_resolver_service";
 export { EntityResolverService } from "./service/entity_resolver_service";
